d8b94addf763cf6ca260cdcc12bf2a37b4445f4b
unknown <kieran6roberts@gmail.com>
2020-11-09 17:52:04 +0100

    added contact form

--- src/heroBtn.js
+++ src/heroBtn.js
@@ -2,11 +2,16 @@ const heroBtn = document.querySelector(".hero__btn");
 const aboutBtn = document.querySelector(".about__toggle");
 const aboutSection = document.querySelector(".about");
 const contactSection = document.querySelector(".contact");
+const wrapper = document.querySelector(".wrapper");
 
 export const toggleAboutHandler = () => {
     contactSection.classList.toggle("open");
     aboutSection.classList.toggle("open");
     document.body.classList.toggle("overflow");
+
+    wrapper.classList.toggle("open");
+
+
 };
 
 heroBtn.addEventListener("click", toggleAboutHandler);
