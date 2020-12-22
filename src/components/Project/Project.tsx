acc8474d988ad4f608be2906ea3a4d2703093e1e
unknown <kieran6roberts@gmail.com>
2020-12-22 16:56:22 +0100

    added new project images

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -32,29 +32,32 @@ export default function Project({
     captions, 
     stackImages, 
     stackNames }: ProjectProps): React.ReactElement {
+    
+        console.log(image)
+        console.log(stackImages)
 
     return (
-        <div className="mb-32 md:px-4 lg:px-12 md:mx-6 bg-light overflow-hidden shadow">
-            <div className="bg-light px-4">
+        <div className="mb-32 overflow-hidden shadow md:px-4 lg:px-12 md:mx-6 bg-light">
+            <div className="px-4 bg-light">
                 <SubHeader title={title}/>
                 <Link href={`projects/${title}`} passHref>
-                    <m.a className="group relative flex justify-center max-w-screen-2xl 2xl:w-max m-auto p-1 mb-8 cursor-pointer focus:outline-none focus:ring-4 focus:ring-yellow-400"
+                    <m.a className="relative flex justify-center p-1 m-auto mb-8 cursor-pointer group max-w-screen-2xl 2xl:w-max focus:outline-none focus:ring-4 focus:ring-yellow-400"
                     aria-label="project case-study"
                         whileHover={{ scale: 1.02, rotate: 1 }}
                         whileTap={{ translateY: -300, rotate: 45 }}
                         transition={{ duration: 0.6 }}>
-                            <div className="absolute top-0 right-0 left-0 bottom-0 bg-purple-400 bg-opacity-90 z-10 transition duration-300 ease-in group-hover:bg-opacity-20"/>
-                            <BiMouse className="absolute z-20 h-12 w-12 top-2/4 -mt-6 text-md text-light transition duration-150 ease-in group-hover:text-transparent animate-pulse" />
+                            <div className="absolute top-0 bottom-0 left-0 right-0 z-10 transition duration-300 ease-in bg-purple-400 bg-opacity-90 group-hover:bg-opacity-20"/>
+                            <BiMouse className="absolute z-20 w-12 h-12 -mt-6 transition duration-150 ease-in top-2/4 text-md text-light group-hover:text-transparent animate-pulse" />
                             <Image src={`/images/${image}`}
                             alt={captions}
                             height={450}
                             width={900}/>
                     </m.a>
                 </Link>
-                <p className="text-xs italic text-dark text-center mb-8">
+                <p className="mb-8 text-xs italic text-center text-dark">
                     {captions}
                 </p>
-                <div className="flex flex-col 2xl:justify-center md:flex-row py-8 2xl:py-32">
+                <div className="flex flex-col py-8 2xl:justify-center md:flex-row 2xl:py-32">
                     <Button link={siteLink}
                     color="bg-light text-dark mb-2 md:mb-0 md:mr-4">
                         visit website
@@ -64,16 +67,16 @@ export default function Project({
                         github repo
                     </Button>
                 </div>
-                <div className="bg-gradient-to-b pl-1 from-purple-400 via-purple-600 to-blue-800">
-                    <h4 className="bg-light text-md text-sec uppercase my-8 2xl:my-24 pl-4">
+                <div className="pl-1 bg-gradient-to-b from-purple-400 via-purple-600 to-blue-800">
+                    <h4 className="pl-4 my-8 uppercase bg-light text-md text-sec 2xl:my-24">
                         project description
                     </h4>
                 </div>
-                <p className="text-xs text-dark py-2">
+                <p className="py-2 text-xs text-dark">
                     {description}
                 </p>
-                <div className="bg-gradient-to-b pl-1 from-purple-400 via-purple-600 to-blue-800">
-                    <h4 className="bg-light text-md text-sec uppercase my-8 2xl:my-24 pl-4">
+                <div className="pl-1 bg-gradient-to-b from-purple-400 via-purple-600 to-blue-800">
+                    <h4 className="pl-4 my-8 uppercase bg-light text-md text-sec 2xl:my-24">
                         tech stack
                     </h4>
                 </div>
@@ -84,7 +87,7 @@ export default function Project({
                             <span className="block pr-4">
                                 case-study
                             </span>
-                            <VscChevronRight className="text-xs transition duration-50 ease-in opacity-0 group-hover:opacity-100"
+                            <VscChevronRight className="text-xs transition ease-in opacity-0 duration-50 group-hover:opacity-100"
                             aria-label="top of the page"/>
                         </a>
                     </Link>
