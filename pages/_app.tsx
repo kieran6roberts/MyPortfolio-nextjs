2268676316c789846b96425d95f0e01e6214ebd8
unknown <kieran6roberts@gmail.com>
2021-01-17 11:25:38 +0100

    updated nav for new blog update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { AnimateSharedLayout } from "framer-motion";
 
-import "src/App.css";
+import "src/styles/app.css";
 import Layout from "../src/components/Layout/Layout";
 
 export default function MyApp({ Component, pageProps, router }: AppProps) {
---
+++ pages/blog.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default function Blog() {
+    return (
+        <div></div>
+    );
+};
